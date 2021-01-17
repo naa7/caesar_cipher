8f2c7461569e03cdd6fc7f9cf1e7b382929d56fb
naa-7 <nalawad000@citymail.cuny.edu>
2021-01-16 22:52:49 -0500

    updated

--- caesar_cipher.py
+++ caesar_cipher.py
@@ -1,13 +1,15 @@
 def caesar_encrypt(clear_text):
     encrypted_output = ""
-    flag = 0
+    # flag = 0
     
     for i in range(len(clear_text)):
+        
+        """
         if clear_text[i].isupper():
             flag = 1
             clear_text[i] = clear_text[i].lower()
         elif clear_text[i] == ' ':
-            encrypted_output = encrypted_output + " "
+            encrypted_output = encrypted_output + "#"
             continue
         elif clear_text[i] == '?':
             encrypted_output = encrypted_output + "!"
@@ -15,19 +17,22 @@ def caesar_encrypt(clear_text):
         elif clear_text[i] == '!':
             encrypted_output = encrypted_output + "?"
             continue
-
+        """
+        
         char_position = ord(clear_text[i])
-        char_position = char_position - 97
+        #char_position = char_position - 97
         new_char_position = char_position + 3
-        new_char_position = new_char_position % 26
-        new_char_position = new_char_position + 97
+
+        # To use only use letters, uncomment next 2 lines
+	    # new_char_position = new_char_position % 26
+        #new_char_position = new_char_position + 97
         new_char = chr(new_char_position)
         
-        if flag == 1:
-           new_char =  new_char.upper()
+        # if flag == 1:
+        #   new_char =  new_char.upper()
         
         encrypted_output = encrypted_output + new_char
-        flag = 0
+        # flag = 0
 
     print("---------------")
     print("Encrypted text:", encrypted_output)
@@ -35,14 +40,16 @@ def caesar_encrypt(clear_text):
 
 def caesar_decrypt(cipher_text):
     decrypted_output = ""
-    flag = 0
+    # flag = 0
     
     for i in range(len(cipher_text)):
+
+        """
         if cipher_text[i].isupper():
             flag = 1
             cipher_text[i] = cipher_text[i].lower()
-        elif cipher_text[i] == ' ':
-            decrypted_output = decrypted_output + " "
+	    elif cipher_text[i] == ' ':
+            decrypted_output = decrypted_output + "#"
             continue
         elif cipher_text[i] == '?':
             decrypted_output = decrypted_output + "!"
@@ -50,19 +57,21 @@ def caesar_decrypt(cipher_text):
         elif cipher_text[i] == '!':
             decrypted_output = decrypted_output + "?"
             continue
-
+        """
+        
         char_position = ord(cipher_text[i])
-        char_position = char_position - 97
+        # char_position = char_position - 97
         new_char_position = char_position - 3
-        new_char_position = new_char_position % 26
-        new_char_position = new_char_position + 97
+        # new_char_position = new_char_position % 26
+        # new_char_position = new_char_position + 97
         new_char = chr(new_char_position)
         
-        if flag == 1:
-            new_char = new_char.upper()
-        
+        # if flag == 1:
+        #   new_char = new_char.upper()
+
         decrypted_output = decrypted_output + new_char
-        flag = 0
+        # flag = 0
+
     print("---------------")
     print("Decrypted text:", decrypted_output)
 
@@ -87,7 +96,9 @@ def main():
                 return 1
             print("-----------------------------------------------")
             answer = input("Do you want to quit?[Y/n]: ")
-            if (answer == 'Y' or answer == 'y') and (answer != 'N' or answer != 'n'):
+            if answer == 'N' or answer == 'n':
+                continue
+            else:
                 print("########################")
                 print("###  End of Program  ###")
                 print("########################")
