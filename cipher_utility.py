93f02670f9b6074e63d8e24f9d29556a9817e3c0
naa-7 <nalawad000@citymail.cuny.edu>
2021-02-25 16:18:57 -0500

    added_monoalphabetic_cipher

--- cipher_utility.py
+++ cipher_utility.py
@@ -1,171 +1,207 @@
-import subprocess
+import subprocess, random
 
 def main():
-    try:
+	try:
 
-        input = subprocess.Popen('zenity --forms --title="Cipher Utility" --text="" --add-combo="Cipher type"\
-                --combo-values="Caesar Cipher|ROT Cipher|Vigenere Cipher" --add-combo="Options" --combo-values="Encrypt|Decrypt" --add-entry="Cipher key" --add-entry="Enter text"', shell=True, stdout=subprocess.PIPE, universal_newlines=True)
+		input = subprocess.Popen('zenity --forms --title="Cipher Utility" --text="" --add-combo="Cipher type"\
+			--combo-values="Caesar Cipher|ROT Cipher|Vigenere Cipher|Monoalphabetic Cipher" --add-combo="Options" --combo-values="Encrypt|Decrypt" --add-entry="Cipher key" --add-entry="Enter text"', shell=True, stdout=subprocess.PIPE, universal_newlines=True)
         
-        input = input.stdout.readline()
-        input = input.strip()
-        ind = index_list(input,'|')
-        cipher = input[:ind[0]]
-        mode = input[ind[0]+1:ind[1]]
-        key = input[ind[1]+1:ind[2]]
-        text = input[ind[2]+1:]
+		input = input.stdout.readline()
+		input = input.strip()
+		ind = index_list(input,'|')
+		cipher = input[:ind[0]]
+		mode = input[ind[0]+1:ind[1]]
+		key = input[ind[1]+1:ind[2]]
+		text = input[ind[2]+1:]
 
-        if cipher == "Caesar Cipher" and text != "":
-           output1 = caesar_cipher(mode,text,int(key))
-
-        elif cipher == "ROT Cipher" and text != "":
-           output1 = rot_cipher(mode,text,int(key))
-
-        elif cipher == "Vigenere Cipher" and text != "":
-           output1 = vigenere_cipher(mode,text,key)
-
-        else:
-            print("#############################")
-            print("### Error, Entry missing! ###")
-            print("#############################")
-            return 1
+		if cipher == "Caesar Cipher" and text != "" and key != "":
+			output1 = caesar_cipher(mode,text,int(key))
+		elif cipher == "ROT Cipher" and text != "" and key != "":
+			output1 = rot_cipher(mode,text,int(key))
+		elif cipher == "Vigenere Cipher" and text != "" and key != "":
+			output1 = vigenere_cipher(mode,text,key)
+		elif cipher == "Monoalphabetic Cipher" and text != "":
+			dictionary = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+			if mode == 'Encrypt':
+				key = getRandomKey(dictionary)
+				output1 = monoalphabetic_cipher(mode,text,key,dictionary)
+				output1 = "Encrypted text: " + output1 + "\nKey: " + key
+			else:
+				if not(keyValidity(key,dictionary)):
+					print("Error found in key")
+					exit(1)
+				output1 = monoalphabetic_cipher(mode,text,key,dictionary)
+		else:
+			print("#############################")
+			print("### Error, Entry missing! ###")
+			print("#############################")
+			return 1
         
-        file = open("temp.txt", "w+")
-        file.write(output1)
-        file.close()
+		file = open("temp.txt", "w+")
+		file.write(output1)
+		file.close()
         
-        if mode == 'Encrypt':
-           output2 = 'cat temp.txt | zenity --width=400 --height=150 --title="Encrypted Text" --text-info && rm temp.txt'
-        else:
-           output2 = 'cat temp.txt | zenity --width=400 --height=150 --title="decrypted Text" --text-info && rm temp.txt'
+		if mode == 'Encrypt':
+			output2 = 'cat temp.txt | zenity --width=400 --height=150 --title="Encrypted Text" --text-info && rm temp.txt'
+		else:
+			output2 = 'cat temp.txt | zenity --width=400 --height=150 --title="decrypted Text" --text-info && rm temp.txt'
 
-        call = subprocess.call(output2, shell=True)
+		call = subprocess.call(output2, shell=True)
 
-
-    except Exception as e:
-        print(e)
-        print("Error, Program Exit!") 
+	except Exception as e:
+		print(e)
+		print("Error, Program Exit!") 
 
 
 def index_list(input,item):
-    start_at = -1
-    indexes = []
+	start_at = -1
+	indexes = []
     
-    while True:
-        try:
-            index = input.index(item,start_at+1)
-        except ValueError:
-            break
-        else:
-            indexes.append(index)
-            start_at = index
-    
-    return indexes
+	while True:
+		try:
+			index = input.index(item,start_at+1)
+		except ValueError:
+			break
+		else:
+			indexes.append(index)
+			start_at = index
+
+	return indexes
+
+
+def getRandomKey(dictionary):
+	key = list(dictionary)
+	random.shuffle(key)
+	return ''.join(key)
+
+
+def keyValidity(key,dictionary):
+	keyList = list(key)
+	dictionaryList = list(dictionary)
+	keyList.sort()
+	dictionaryList.sort()
+	return keyList == dictionaryList
 
 
 def caesar_cipher(mode,text,key):
-    output = ""
+	output = ""
     
-    for i in range(len(text)):
-        #Obtain the ASCII value using ord
-        char_position = ord(text[i])
-        char_position = char_position - 32
+	for i in range(len(text)):
+		#Obtain the ASCII value using ord
+		char_position = ord(text[i])
+		char_position = char_position - 32
         
-        if mode == 'Encrypt':
-           new_char_position = char_position + key
-        elif mode == 'Decrypt':
-           new_char_position = char_position - key
+		if mode == 'Encrypt':
+			new_char_position = char_position + key
+		elif mode == 'Decrypt':
+			new_char_position = char_position - key
         
-        new_char_position = new_char_position % 94
-        new_char_position = new_char_position + 32
-        new_char = chr(new_char_position)
-        output = output + new_char
+		new_char_position = new_char_position % 94
+		new_char_position = new_char_position + 32
+		new_char = chr(new_char_position)
+		output = output + new_char
 
-    return output
+	return output
 
 
 def rot_cipher(mode,text,key):
-    output = ""
-    flag = 0
+	output = ""
 
-    for i in range(len(text)):
-        letter = text[i]
+	for i in range(len(text)):
+		letter = text[i]
         
-        if letter.isupper() == True:
-           letter = letter.lower()
-           flag = 1
+		if letter.isupper() == True:
+			letter = letter.lower()
         
-        char_position = ord(letter)
+		char_position = ord(letter)
         
-        if not((char_position >= 65 and char_position <= 90) or (char_position >= 97 and char_position <= 122)):
-           output = output + chr(char_position)
-           continue
+		if not((char_position >= 65 and char_position <= 90) or (char_position >= 97 and char_position <= 122)):
+			output = output + chr(char_position)
+			continue
 
-        char_position = char_position - 97
+		char_position = char_position - 97
         
-        if mode == 'Encrypt':
-           new_char_position = char_position + key
-        elif mode == 'Decrypt':
-           new_char_position = char_position - key
+		if mode == 'Encrypt':
+			new_char_position = char_position + key
+		elif mode == 'Decrypt':
+			new_char_position = char_position - key
         
-        new_char_position = new_char_position % 26
-        new_char_position = new_char_position + 97
-        new_char = chr(new_char_position)
+		new_char_position = new_char_position % 26
+		new_char_position = new_char_position + 97
+		new_char = chr(new_char_position)
         
-        if flag == 1:
-           new_char = new_char.upper()
-           flag = 0
-        
-        output = output + new_char
+		if text[i].isupper():
+			new_char = new_char.upper()
 
-    return output
+		output = output + new_char
+
+	return output
 
 def vigenere_cipher(mode,text,key):
-    output = ""
-    keyIndex = 0
-    flag = 0
-    key = key.upper()
+	output = ""
+	keyIndex = 0
+	key = key.upper()
     
-    for i in range(len(text)):
-        text_letter = text[i]
-        key_letter = key[keyIndex]
+	for i in range(len(text)):
+		text_letter = text[i]
+		key_letter = key[keyIndex]
         
-        if text_letter.islower() == True:
-           text_letter = text_letter.upper()
-           flag = 1
+		if text_letter.islower() == True:
+			text_letter = text_letter.upper()
         
-        char_position = ord(text_letter)
-        key_position = ord(key_letter)
+		char_position = ord(text_letter)
+		key_position = ord(key_letter)
 
-        if not((char_position >= 65 and char_position <= 90) or (char_position >= 97 and char_position <= 122)):
-           output = output + chr(char_position)
-           continue
+		if not((char_position >= 65 and char_position <= 90) or (char_position >= 97 and char_position <= 122)):
+			output = output + chr(char_position)
+			continue
 
-        char_position = char_position - 65
+		char_position = char_position - 65
         
-        if mode == 'Encrypt':
-           new_char_position = char_position + key_position
-        elif mode == 'Decrypt':
-           new_char_position = char_position - key_position
+		if mode == 'Encrypt':
+			new_char_position = char_position + key_position
+		elif mode == 'Decrypt':
+			new_char_position = char_position - key_position
 
-        new_char_position = new_char_position % 26
-        new_char_position = new_char_position + 65
-        new_char = chr(new_char_position)
-        keyIndex = keyIndex + 1
+		new_char_position = new_char_position % 26
+		new_char_position = new_char_position + 65
+		new_char = chr(new_char_position)
+		keyIndex = keyIndex + 1
 
-        if keyIndex == len(key):
-           keyIndex = 0
+		if keyIndex == len(key):
+			keyIndex = 0
         
-        if flag == 1:
-           new_char = new_char.lower()
-           flag = 0
+		if text[i].islower():
+			new_char = new_char.lower()
         
-        output = output + new_char
+		output = output + new_char
 
-    return output
+	return output
+
     
+def monoalphabetic_cipher(mode,text,key,dictionary):
+	output = ""
+	letters = dictionary
+	keys = key
+
+	if mode == 'Decrypt':
+		letters, keys = keys, letters
+
+	for i in range(len(text)):
+		if text[i].upper() in letters:
+			index = letters.find(text[i].upper())
+			if text[i].isupper():
+				output = output + keys[index].upper()
+			else:
+				output = output + keys[index].lower()
+		else:
+			output = output + text[i]
+
+	return output
+
 
 if __name__ == "__main__":
-    main()
+	main()
 
 
 
