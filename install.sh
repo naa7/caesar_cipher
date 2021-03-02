5089eb09c02925a8c98be3b5b3d880c024b4f398
naa-7 <nalawad000@citymail.cuny.edu>
2021-03-02 15:59:55 -0500

    updated

--- install.sh
+++ install.sh
@@ -5,7 +5,10 @@ sudo cp cipher_utility /usr/bin/
 
 # Modify the path to `icon.png` in cipher_utility.desktop
 path=$PWD
-echo "Icon=$path/images/icon.png" >> cipher_utility.desktop
+if !([[ $(grep -o "Icon" cipher_utility.desktop) == "Icon" ]])
+then
+	echo "Icon=$path/images/icon.png" >> cipher_utility.desktop
+fi
 
 # Copy `cipher_utility.desktop` to `/usr/share/applications/`
 sudo cp cipher_utility.desktop /usr/share/applications/
