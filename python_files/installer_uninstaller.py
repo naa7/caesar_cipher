542baa32a596e8398c2aec836687ae78b6f2ef76
naa-7 <nalawad000@citymail.cuny.edu>
2021-03-05 04:09:46 -0500

    updated

--- python_files/installer_uninstaller.py
+++ python_files/installer_uninstaller.py
@@ -4,14 +4,13 @@ def main():
 
 	answer = input("Do you want to [I]NSTALL or [U]NINSTALL the program?(I/u): ")
 	if answer == 'I' or answer == 'i':
-		install_1 = 'echo -ne "\033[A\033[2K\r";sleep 1;sudo cp cipher_utility /usr/bin/'
-		install_2 = 'echo -e "[Desktop Entry]\nName=Cipher Utility\nStartupWMClass=Cipher Utility\nComment=Encryption/Decryption Utility\nExec=/usr/bin/cipher_utility\nType=Application\nCategories=Utility;\n" > cipher_utility.desktop'
-		install_3 = 'path=$PWD;if !([[ $(grep -o "Icon" cipher_utility.desktop) == "Icon" ]]);then echo "Icon=$path/icon.png" >> cipher_utility.desktop;fi;sudo cp cipher_utility.desktop /usr/share/applications/;rm cipher_utility.desktop'	
-		call_1 = subprocess.call(install_1, shell=True)
-		call_2 = subprocess.call(install_2, shell=True)
-		call_3 = subprocess.call(install_3, shell=True)
+		install = 'echo -ne "\033[A\033[2K\r";mkdir Cipher-Utility; cp -r $(ls -A | grep -v "Cipher-Utility") Cipher-Utility/;mv Cipher-Utility cipher-utility;mv cipher-utility ~/;\
+		sudo cp cipher_utility /usr/bin/;cd ~/cipher-utility &&\
+		echo -e "[Desktop Entry]\nName=Cipher Utility\nStartupWMClass=Cipher Utility\nComment=Encryption/Decryption Utility\nExec=/usr/bin/cipher_utility\nType=Application\nCategories=Utility" > cipher_utility.desktop;\
+		path=$PWD;if !([[ $(grep -o "Icon" cipher_utility.desktop) == "Icon" ]]);then echo "Icon=$path/icon.png" >> cipher_utility.desktop;fi;sudo cp cipher_utility.desktop /usr/share/applications/;rm cipher_utility.desktop'
+		call = subprocess.call(install, shell=True)
 	elif answer == 'U' or answer == 'u':
-		uninstall = 'echo -ne "\033[A\033[2K\r";sleep 1;sudo rm /usr/bin/cipher_utility >/dev/null 2>&1 && sudo rm /usr/share/applications/cipher_utility.desktop >/dev/null 2>&1'
+		uninstall = 'echo -ne "\033[A\033[2K\r";sleep 1;sudo rm /usr/bin/cipher_utility >/dev/null 2>&1 && sudo rm /usr/share/applications/cipher_utility.desktop >/dev/null 2>&1 && rm -rf ~/cipher-utility >/dev/null 2>&1'
 		call = subprocess.call(uninstall, shell=True)
 	else:
 		line_removal = 'echo -ne "\033[A\033[2K\r"'
